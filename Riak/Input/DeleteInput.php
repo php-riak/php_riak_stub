6bc594a4eee27141395807bc68bea710661fdce9
Kaspar Pedersen <kpn@trifork.com>
2013-06-26 16:45:41 +0200

    Put&delete inputs

--- Riak/Input/DeleteInput.php
+++ Riak/Input/DeleteInput.php
@@ -2,7 +2,117 @@
 
 namespace Riak\Input;
 
+/**
+ * Class DeleteInput
+ * @package Riak\Input
+ * Optional extra input for a riak delete request.
+ */
 class DeleteInput extends Input
 {
+    /**
+     * @var null|int
+     */
+    protected $r = null;
+
+    /**
+     * @var null|int
+     */
+    protected $rw = null;
+
+    /**
+     * @var null|int
+     */
+    protected $pr = null;
+
+    /**
+     * @var null|int
+     */
+    protected $w = null;
+
+    /**
+     * @var null|int
+     */
+    protected $dw = null;
+
+    /**
+     * @var null|int
+     */
+    protected $pw = null;
+
+    /**
+     * @var null|string
+     */
+    protected $vClock = null;
+
+    /** Set DW value
+     * @param int $dw
+     */
+    public function setDW($dw) {}
+
+    /** Get DW value
+     * @return int|null
+     */
+    public function getDW() {}
+
+    /** Set PR value
+     * @param int $pr
+     */
+    public function setPR($pr) {}
+
+    /** Get PR Value
+     * @return int|null
+     */
+    public function getPR() {}
+
+    /** Set PW value
+     * @param int $pw
+     */
+    public function setPW($pw) {}
+
+    /** Get PW value
+     * @return int|null
+     */
+    public function getPW() {}
+
+    /** Set R value
+     * @param int $r
+     */
+    public function setR($r) {}
+
+    /** Get R value
+     * @return int|null
+     */
+    public function getR() {}
+
+    /** Set RW value
+     * @param int $rw
+     */
+    public function setRW($rw) {}
+
+    /** Get RW value
+     * @return int|null
+     */
+    public function getRW() {}
+
+    /** Set vclock
+     * @param string $vClock
+     */
+    public function setVClock($vClock) {}
+
+    /** Get vclock
+     * @return null|string
+     */
+    public function getVClock() {}
+
+    /** Set W value
+     * @param int $w
+     */
+    public function setW($w) {}
+
+    /** Get W value
+     * @return int|null
+     */
+    public function getW() {}
+
 
 }
--- Riak/Input/PutInput.php
+++ Riak/Input/PutInput.php
@@ -2,7 +2,132 @@
 
 namespace Riak\Input;
 
+/**
+ * Class PutInput
+ * @package Riak\Input
+ * Optional extra input for a riak put request.
+ */
 class PutInput extends Input
 {
+    /**
+     * @var null|int
+     */
+    protected $w = null;
+
+    /**
+     * @var null|int
+     */
+    protected $dw = null;
+
+    /**
+     * @var null|int
+     */
+    protected $pw = null;
+
+    /**
+     * @var null|string
+     */
+    protected $vClock = null;
+
+    /**
+     * @var null|bool
+     */
+    protected $returnHead = null;
+
+    /**
+     * @var null|bool
+     */
+    protected $returnBody = null;
+
+    /**
+     * @var null|bool
+     */
+    protected $ifNoneMatch = null;
+
+    /**
+     * @var null|bool
+     */
+    protected $ifNotModified = null;
+
+    /** Set DW value
+     * @param int $dw
+     */
+    public function setDW($dw) {}
+
+    /** Get DW value
+     * @return int|null
+     */
+    public function getDW() {}
+
+    /** Set if none match
+     * @param bool $ifNoneMatch
+     */
+    public function setIfNoneMatch($ifNoneMatch) {}
+
+    /** Get if none match
+     * @return bool|null
+     */
+    public function getIfNoneMatch() {}
+
+    /** Set if not modified
+     * @param bool $ifNotModified
+     */
+    public function setIfNotModified($ifNotModified) {}
+
+    /** Get if not modified
+     * @return bool|null
+     */
+    public function getIfNotModified() {}
+
+    /** Set PW value
+     * @param int|null $pw
+     */
+    public function setPW($pw) {}
+
+    /** Get PW value
+     * @return int|null
+     */
+    public function getPw() {}
+
+    /** Set return body
+     * @param bool $returnBody
+     */
+    public function setReturnBody($returnBody) {}
+
+    /** Get return body
+     * @return bool|null
+     */
+    public function getReturnBody() {}
+
+    /** Set return head
+     * @param bool $returnHead
+     */
+    public function setReturnHead($returnHead) {}
+
+    /** Get return head
+     * @return bool|null
+     */
+    public function getReturnHead() {}
+
+    /** Set VClock
+     * @param string $vClock
+     */
+    public function setVClock($vClock) {}
+
+    /** Get VClock
+     * @return null|string
+     */
+    public function getVClock() {}
+
+    /** Set W value
+     * @param int $w
+     */
+    public function setW($w) {}
+
+    /** Get W value
+     * @return int|null
+     */
+    public function getW() {}
+
 
 }
