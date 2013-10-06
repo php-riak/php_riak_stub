064db3b09cd3d428f24098123c5e736464865a51
Kaspar Pedersen <kaspar@inka-apps.com>
2013-10-06 19:51:48 +0200

    Update to latest php_riak master

--- Riak/BucketPropertyList.php
+++ Riak/BucketPropertyList.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Riak;
+use Riak\Property\CommitHookList;
+use Riak\Property\ModuleFunction;
 
 /**
  * Class BucketPropertyList
@@ -9,21 +11,12 @@ namespace Riak;
  */
 class BucketPropertyList
 {
-    /**
-     * @var bool allow mults in bucket
-     */
-    private $allowMult;
-
-    /**
-     * @var int n value for bucket
-     */
-    private $nVal;
 
     /**
      * @param $nVal int n value
      * @param $allowMult bool allow mults
      */
-    public function __construct($nVal, $allowMult) {}
+    public function __construct($nVal = null, $allowMult = null) {}
 
     /** Set allow mult for bucket
      * @param boolean $allowMult
@@ -47,5 +40,229 @@ class BucketPropertyList
      */
     public function getNValue() {}
 
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set last write wins
+     * @param boolean $lastWriteWins
+     * @return BucketPropertyList
+     */
+    public function setLastWriteWins($lastWriteWins) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Get last write wins
+     * @return boolean|null
+     */
+    public function getLastWriteWins() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set old vclock value
+     * @param int $oldVClock
+     * @return BucketPropertyList
+     */
+    public function setOldVClock($oldVClock) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getOldVClock() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set young vclock value
+     * @param int $youngVClock
+     * @return BucketPropertyList
+     */
+    public function setYoungVClock($youngVClock) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getYoungVClock() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set small vclock value
+     * @param int $smallVClock
+     * @return BucketPropertyList
+     */
+    public function setSmallVClock($smallVClock) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getSmallVClock() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set big vclock value
+     * @param int $smallVClock
+     * @return BucketPropertyList
+     */
+    public function setBigVClock($smallVClock) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getBigVClock() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set R value
+     * @return BucketPropertyList
+     * @param int $r
+     */
+    public function setR($r) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getR() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set PR value
+     * @param int $r
+     * @return BucketPropertyList
+     */
+    public function setPR($r) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getPR() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set W value
+     * @param int $dw
+     * @return BucketPropertyList
+     */
+    public function setW($dw) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getW() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set DW value
+     * @param int $dw
+     * @return BucketPropertyList
+     */
+    public function setDW($dw) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getDW() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set PW value
+     * @param int $dw
+     * @return BucketPropertyList
+     */
+    public function setPW($dw) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getPW() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * Set RW value
+     * @param int $dw
+     * @return BucketPropertyList
+     */
+    public function setRW($dw) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return int|null
+     */
+    public function getRW() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @param bool $basicQuorum
+     * @return BucketPropertyList
+     */
+    public function setBasicQuorum($basicQuorum) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return bool|null
+     */
+    public function getBasicQuorum() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @param bool $notFoundOk
+     * @return BucketPropertyList
+     */
+    public function setNotFoundOk($notFoundOk) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return bool|null
+     */
+    public function getNotFoundOk() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @param bool $search
+     * @return BucketPropertyList
+     */
+    public function setSearchEnabled($search) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return bool|null
+     */
+    public function getSearchEnabled() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @param string $backend
+     * @return BucketPropertyList
+     */
+    public function setBackend($backend) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return string|null
+     */
+    public function getBackend() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @param CommitHookList $preCommitHookList
+     * @return BucketPropertyList
+     */
+    public function setPreCommitHookList(CommitHookList $preCommitHookList) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return CommitHookList|null
+     */
+    public function getPreCommitHookList() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @param CommitHookList $postCommitHookList
+     * @return BucketPropertyList
+     */
+    public function setPostCommitHookList(CommitHookList $postCommitHookList) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return CommitHookList|null
+     */
+    public function getPostCommitHookList() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @param ModuleFunction $linkFun
+     */
+    public function setLinkFun(ModuleFunction $linkFun) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return ModuleFunction|null
+     */
+    public function getLinkFun() {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @param ModuleFunction $cHashKeyFun
+     */
+    public function setCHashKeyFun(ModuleFunction $cHashKeyFun) {}
+
+    /** IMPORTANT Only supported by riak 1.4+
+     * @return ModuleFunction|null
+     */
+    public function getCHashKeyFun() {}
+
+        // Riak 1.4+ properties
+    /* TODO
+    uint8_t replication_mode_use;
+    enum RIACK_REPLICATION_MODE replication_mode;
+     * */
 
 }
---
+++ Riak/Property/CommitHook.php
@@ -0,0 +1,56 @@
+<?php
+/*
+   Copyright 2013: Kaspar Bach Pedersen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+namespace Riak\Property;
+
+/**
+ * Class CommitHook
+ * @package Riak\Property
+ */
+class CommitHook {
+    /**
+     * @param string $moduleOrName erlang module name, or javascript function name
+     * @param null|string $function erlang function name
+     */
+    public function __construct($moduleOrName, $function=null) {}
+
+    /**
+     * @return string|null
+     */
+    public function getJsName() {}
+
+    /**
+     * @return string|null
+     */
+    public function getErlModule() {}
+
+    /**
+     * @return string|null
+     */
+    public function getErlFunction() {}
+
+    /**
+     * @return bool
+     */
+    public function isJavascript() {}
+
+    /**
+     * @return bool
+     */
+    public function isErlang() {}
+
+}
