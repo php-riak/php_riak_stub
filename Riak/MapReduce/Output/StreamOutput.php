db618769f6a59aefacfbb500dd687dcd37763507
Kaspar Pedersen <kaspar@inka-apps.com>
2013-11-17 20:07:32 +0100

    Remove stream warnings as the problem have been resolved

--- Riak/MapReduce/Output/StreamOutput.php
+++ Riak/MapReduce/Output/StreamOutput.php
@@ -5,9 +5,6 @@ namespace Riak\MapReduce\Output;
 interface StreamOutput {
     /**
      * Got a response from riak
-     * WARNING!! If you perform operations on riak in this callback you need to do it
-     * on a different connection than the one running the mapreduce.
-     * See https://github.com/TriKaspar/php_riak/issues/52 for more info.
      * @param Output $response
      * @return void
      */
--- Riak/Output/KeyStreamOutput.php
+++ Riak/Output/KeyStreamOutput.php
@@ -4,9 +4,6 @@ namespace Riak\Output;
 interface KeyStreamOutput
 {
     /**
-     * WARNING!! If you perform operations on riak in this callback you need to do it
-     * on a different connection than the one running the key list.
-     * See https://github.com/TriKaspar/php_riak/issues/52 for more info.
      * @param string $key received a key from riak
      * @return void
      */
